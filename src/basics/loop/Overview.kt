055b0fa79483677d03ebfa979608b7bdf71c495d
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-02-13 00:35:37 +0300

    Sample codes were written for primitive type variables in the Overview class.

--- src/basics/loop/Overview.kt
+++ src/basics/loop/Overview.kt
@@ -3,16 +3,45 @@ package basics.loop
 // Code with ♥️
 // Created by Oguzhan OZGOKCE
 
-/*
-primitive data types in Kotlin
-Int, Long, Short, Byte, Double, Float, Char, Boolean
-özel optimasyonla byte code cevriliyor primitive tiplerde
-Kotlin de her şey bir class gibi davranır bir nesne gibi davranır. Ama Primitive tiplerde özel bir optimizasyon yapılara byte code cevrilir.
-*/
-
 class Overview {
+
     val name: String = "Oguzhan"
     val age: Int = 25
     val isMale: Boolean = true
     val firstLetterOfName: Char = 'O'
+
+    var width = 100
+    var height = 200
+    var length = 300
+    var usedSpace = 0
+
+    val availableSpace : Int
+        get() {
+            return  (width * height * length) - usedSpace
+        }
 }
+
+/**
+ * Kotlin de iki tür değişken vardır:
+ * primitive tipli değişkenler ve referans tipli değişkenler
+ * primitive tipler: Int, Long, Short, Byte, Double, Float, Char, Boolean
+ * referans tipler: String, Array, Class
+ */
+
+// Kotlin de her şey bir class' sa biz primitive tiplere sahip degilmiyiz?
+/**
+ * Class gibi gözüken primitive tiplerde özel optimazyonla byte code cevriliyor.
+ * val age: Int = 25  arka planda java da int e cevriliyor.
+ * Not: Bu tip dönüsümü degildir.
+ */
+
+/**
+ * val için genelde inMutable degisken denir. Yani degeri degistirilemez. Fakat bu yanlis bir tanimdir.
+ * Dogrusu read-only degiskenlerdir. Yani ilk deger atamasından sonra tekrar deger atanamaz.
+ * val availableSpace : Int
+ *         get() {
+ *             return  (width * height * length) - usedSpace
+ *         }
+ * burada availableSpace degiskeni degistirilemez degildir. Return degerine göre degeri degisebilir.
+ */
+
